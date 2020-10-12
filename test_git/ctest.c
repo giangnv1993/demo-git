c43f2a163b50a63aeafd2f7d5f0b6919eeccb941
giangnv <giangnv1704@gmail.com>
2020-10-12 12:44:16 +0700

    add dev1 function

--- test_git/ctest.c
+++ test_git/ctest.c
@@ -2,20 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-int a[15];
+void dev1(void);
 
 int main()
 {
-    int ui = 0;
-    for (ui = 0; ui < 14; ui ++)
-    {
-        a[ui] = ui;
-         printf("a[%d] = %d\r\n", ui, a[ui]);
-    }
-
-    int N;
-    printf("nhap vao so N = ");
-    scanf("%d", &N);
+    dev1();
        
     return 0;
+}
+
+
+void dev1(void)
+{
+    printf("dev1\r\b");
 }
